310bafaa9a4989d6e6daeb4ca7b7883d2121cbdb
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-07-24 15:29:01 -0300

    mero teste

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.1.0'
 import xmlschema
 from pprint import pprint
 
-nfe_v4_schema_file = open('D:/XML_NFe_to_SQL/xml_nfe_to_sql/resourse/xsd/NFe/v3.10/enviNFe_v3.10.xsd')
+nfe_v4_schema_file = open('D:/XML_NFe_to_SQL/xml_nfe_to_sql/resourse/xsd/NFe/v3.10/procNFe_v3.10.xsd')
 
 nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= 'D:/XML_NFe_to_SQL/xml_nfe_to_sql/resourse/xsd/NFe/v3.10/' )
 
@@ -16,9 +16,23 @@ nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= 'D:/XML_NFe_to_SQ
 
 # # print (nfe_schema.validate('D:/OneDrive/Documentos/TRABALHO/AFE 07/Ressarcimento ICMS/E04 079 1502 2018 APPLE COMPUTER BRASIL LTDA - rest de ICMS ST pago a maior - erro de calculo/NFEs dez-2017/NFE_35171200623904000335550010036024861420964251.xml'))
 
-for xsd_component in nfe_schema.iter_components():
+for xsd_component in nfe_schema.iter_globals():
     pprint (xsd_component)
 
 print('teste de run do arquvo __init__.py')
+
+print('========================================\n')
+
+nfeProc_element = nfe_schema.elements['nfeProc']
+print('nfeProc_element: ')
+pprint(nfeProc_element.schema)
+print('nfeProc_elem:')
+pprint(nfeProc_element.elem)
+print('nfeProc_elem.attribute:')
+pprint(nfeProc_element.attributes['type'])
+
+
+
+
 
 
