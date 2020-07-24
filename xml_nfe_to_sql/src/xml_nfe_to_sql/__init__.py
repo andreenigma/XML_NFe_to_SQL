3d38800b24995a0a22760d5db5d3da1a5a1d0c0f
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-07-24 18:10:32 -0300

    ASDGASGH

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -4,17 +4,12 @@ __version__ = '0.1.0'
 import xmlschema
 from pprint import pprint
 
-nfe_v4_schema_file = open('D:/XML_NFe_to_SQL/xml_nfe_to_sql/resourse/xsd/NFe/v3.10/procNFe_v3.10.xsd')
+nfe_v4_schema_file = open('./resourse/xsd/NFe/v3.10/procNFe_v3.10.xsd')
 
-nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= 'D:/XML_NFe_to_SQL/xml_nfe_to_sql/resourse/xsd/NFe/v3.10/' )
+nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= './resourse/xsd/NFe/v3.10/' )
 
-# # nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file)
+nfe_schema_tabular = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= './resourse/xsd/NFe/v3.10/', converter= xmlschema.ColumnarConverter)
 
-# # print (nfe_schema.is_valid('D:/OneDrive/Documentos/TRABALHO/AFE 07/Ressarcimento ICMS/E04 079 1502 2018 APPLE COMPUTER BRASIL LTDA - rest de ICMS ST pago a maior - erro de calculo/NFEs dez-2017/NFE_35171200623904000335550010036024861420964251.xml'))
-
-# # pprint (nfe_schema.to_dict('D:/OneDrive/Documentos/TRABALHO/AFE 07/Ressarcimento ICMS/E04 079 1502 2018 APPLE COMPUTER BRASIL LTDA - rest de ICMS ST pago a maior - erro de calculo/NFEs dez-2017/NFE_35171200623904000335550010036024861420964251.xml'))
-
-# # print (nfe_schema.validate('D:/OneDrive/Documentos/TRABALHO/AFE 07/Ressarcimento ICMS/E04 079 1502 2018 APPLE COMPUTER BRASIL LTDA - rest de ICMS ST pago a maior - erro de calculo/NFEs dez-2017/NFE_35171200623904000335550010036024861420964251.xml'))
 
 for xsd_component in nfe_schema.iter_globals():
     pprint (xsd_component)
@@ -23,13 +18,17 @@ print('teste de run do arquvo __init__.py')
 
 print('========================================\n')
 
+pprint(nfe_schema_tabular.to_dict)
+
+print('========================================\n')
+
 nfeProc_element = nfe_schema.elements['nfeProc']
-print('nfeProc_element: ')
+print('nfeProc_element.schema: ')
 pprint(nfeProc_element.schema)
-print('nfeProc_elem:')
+print('nfeProc_element.elem:')
 pprint(nfeProc_element.elem)
 print('nfeProc_elem.attribute:')
-pprint(nfeProc_element.attributes['type'])
+pprint(nfeProc_element.attributes)
 
 
 
