3e12499c06766941749edee3d2a9c61a209d6bc7
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-07-24 20:05:38 -0300

    asdgf

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -3,24 +3,28 @@ __version__ = '0.1.0'
 # import Leitor_XML
 import xmlschema
 from pprint import pprint
+import pandas as pd
 
 nfe_v4_schema_file = open('./resourse/xsd/NFe/v3.10/procNFe_v3.10.xsd')
 
-nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= './resourse/xsd/NFe/v3.10/' )
-
-nfe_schema_tabular = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= './resourse/xsd/NFe/v3.10/', converter= xmlschema.ColumnarConverter)
+nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= './resourse/xsd/NFe/v3.10/', converter=xmlschema.ParkerConverter )
 
 
-for xsd_component in nfe_schema.iter_globals():
+
+
+print('______________________________________\n')
+for xsd_component in nfe_schema.iter_components():    
     pprint (xsd_component)
+    print('______________________________________\n')
 
-print('teste de run do arquvo __init__.py')
+# print('teste de run do arquvo __init__.py')
 
-print('========================================\n')
+# print('========================================\n')
 
-pprint(nfe_schema_tabular.to_dict)
+# print('ColumnarConverter: \n')
+# pprint(nfe_schema.to_dict(nfe_xml_document, dict_class=dict, indent=4))
 
-print('========================================\n')
+# print('========================================\n')
 
 nfeProc_element = nfe_schema.elements['nfeProc']
 print('nfeProc_element.schema: ')
@@ -29,6 +33,10 @@ print('nfeProc_element.elem:')
 pprint(nfeProc_element.elem)
 print('nfeProc_elem.attribute:')
 pprint(nfeProc_element.attributes)
+
+det = nfe_schema.elements['nfeProc']
+print('det.maps: ')
+pprint(nfe_schema.maps.elements[det.qualified_name])
 
 
 
