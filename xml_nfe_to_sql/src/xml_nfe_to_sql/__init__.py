d94ca83e8aa6a92f2bab9ded388bbc966168e63d
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-09-01 23:31:40 -0300

    decorators de criação de bd

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -14,97 +14,62 @@ nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= nfe_v4_base_url )
 
 component_list = nfe_schema.findall('.//*')
    
-# print('\n****all components:\n')
 
-# for comp in component_list:    
+# def schema_iterate(schema: xmlschema.XsdComponent, handler=None):
 
-#     if comp.type.is_complex(): 
+#     component_list = schema.findall('.//*')
 
-#         print('Crida a tabela: ', comp.local_name)
-#         print('com os seguintes campos: ') 
-#         # print('\tcampo(chave_secundária):', comp.parent.)  
-
-#         for sub_comp in comp.iter_components(xsd_classes=xmlschema.XsdElement):
-
-#            if sub_comp.type.is_simple():
-
-#                print('\tcampo: ', sub_comp.local_name)
-
-
-
-# class BaseHandler:
-#     def __init__(self, stream):
-#         self._stream_obj = stream
-
-#     def 
-
-
-
-def schema_iterate(schema: xmlschema.XsdComponent, handler=None):
-
-    component_list = schema.findall('.//*')
-
-    # for component in schema.iter_components(xsd_classes=xmlschema.XsdElement):
-    for component in component_list:
-        if component.type.is_complex():          
-            columns_components = []
-            complex_content = []
+#     # for component in schema.iter_components(xsd_classes=xmlschema.XsdElement):
+#     for component in component_list:
+#         if component.type.is_complex():          
+#             columns_components = []
+#             complex_content = []
             
-            # for sub_comp in component.iter_components(xsd_classes=xmlschema.XsdElement):
-            for sub_comp in component.iterchildren():
-                if sub_comp.type.is_simple():
-                    columns_components.append(sub_comp)
+#             # for sub_comp in component.iter_components(xsd_classes=xmlschema.XsdElement):
+#             for sub_comp in component.iterchildren():
+#                 if sub_comp.type.is_simple():
+#                     columns_components.append(sub_comp)
 
-                if sub_comp.type.is_complex():
-                    complex_content.append(sub_comp)
+#                 if sub_comp.type.is_complex():
+#                     complex_content.append(sub_comp)
             
-            print('Tipo complexo de nome ', component.local_name)
+#             print('Tipo complexo de nome ', component.local_name)
 
-            if component.attributes:
-                print('tem os seguintes atributos:')
-                for att in component.attributes:
-                    print(att)
+#             if component.attributes:
+#                 print('tem os seguintes atributos:')
+#                 for att in component.attributes:
+#                     print(att)
 
           
-            if columns_components:               
-                print('Criar tabela com os seguintes campos: ')
+#             if columns_components:               
+#                 print('Criar tabela com os seguintes campos: ')
                 
-                for s_comp in columns_components:
-                    print('\t', s_comp.local_name)
+#                 for s_comp in columns_components:
+#                     print('\t', s_comp.local_name)
 
-            if complex_content:               
-                print('\t contem os seguintes tipos complexos: ')
+#             if complex_content:               
+#                 print('\t contem os seguintes tipos complexos: ')
                 
-                for c_comp in complex_content:
-                    print('\t \t', c_comp.local_name)
+#                 for c_comp in complex_content:
+#                     print('\t \t', c_comp.local_name)
             
 
+# schema_iterate(nfe_schema)
 
-schema_iterate(nfe_schema)
+class DbBuilder:
+    
+    def create_table(self, name: str, fields = {}):
+        #experimental para testes
+        print('Contruindo a tabela {table}'.format(table = name))
 
-# for comp in nfe_schema.iter_components(xsd_classes=xmlschema.XsdElement):    
-
-#     if comp.type.is_complex(): 
-
-#         print('Crida a tabela: ', comp.local_name)
-#         print('com os seguintes campos: ') 
-#         # print('\tcampo(chave_secundária):', comp.parent.)  
-
-#         for sub_comp in comp.iter_components(xsd_classes=xmlschema.XsdElement):
-
-#            if sub_comp.type.is_simple():
-
-#                print('\tcampo: ', sub_comp.local_name)
-
-#             elif sub_comp.type.is_complex():
- 
+    def create_field(self, table_name: str, field_name: str, type_name: str, type_length):
+        #experimental para testes
+        print('Criado o campo {field} na seguinte tabela {table}'.format(field=field_name,
+        table= table_name ))
 
 
+def create_tables(xml_schema: xmlschema.XMLSchema, db_builder):
+    component_list = xml_schema.findall('.//*')
 
-
-
-
-
-
-
-
+    # varredura dos componentes
+    for schema_component in component_list:
