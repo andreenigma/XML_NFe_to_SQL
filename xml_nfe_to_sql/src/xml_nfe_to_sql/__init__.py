4a372a93bc6b7b349371d83e18e09370ce77d284
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-09-15 22:27:29 -0300

    Inicio da implementação do padrão Decorator para parser do xmlschema

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -1,10 +1,12 @@
 __version__ = '0.1.0'
 
 # import Leitor_XML
+import abc
 import xmlschema
 from xmlschema import helpers
 from pprint import pprint
 import pandas as pd
+import sqlalchemy
 
 
 nfe_v4_schema_file = open('./resourse/xsd/NFe/v3.10/nfe_v3.10.xsd')
@@ -15,61 +17,41 @@ nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= nfe_v4_base_url )
 component_list = nfe_schema.findall('.//*')
    
 
-# def schema_iterate(schema: xmlschema.XsdComponent, handler=None):
+class Handler:
 
-#     component_list = schema.findall('.//*')
+    @abc.abstractmethod
+    def create_table(table_name: str):
+        pass
 
-#     # for component in schema.iter_components(xsd_classes=xmlschema.XsdElement):
-#     for component in component_list:
-#         if component.type.is_complex():          
-#             columns_components = []
-#             complex_content = []
-            
-#             # for sub_comp in component.iter_components(xsd_classes=xmlschema.XsdElement):
-#             for sub_comp in component.iterchildren():
-#                 if sub_comp.type.is_simple():
-#                     columns_components.append(sub_comp)
+    @abc.abstractmethod
+    def create_field(table_name: str, field: str, type: str):
+        pass
 
-#                 if sub_comp.type.is_complex():
-#                     complex_content.append(sub_comp)
-            
-#             print('Tipo complexo de nome ', component.local_name)
+    @abc.abstractmethod
+    def delete_table(table_name: str):
+        pass
 
-#             if component.attributes:
-#                 print('tem os seguintes atributos:')
-#                 for att in component.attributes:
-#                     print(att)
+    @abc.abstractmethod
+    def delete_field(table_name: str, field: str)
 
-          
-#             if columns_components:               
-#                 print('Criar tabela com os seguintes campos: ')
-                
-#                 for s_comp in columns_components:
-#                     print('\t', s_comp.local_name)
+    @abc.abstractmethod
+    def relationship(table_name: str, foreing_key: str, foreing_table: str, referenced_field: str):
+        pass
 
-#             if complex_content:               
-#                 print('\t contem os seguintes tipos complexos: ')
-                
-#                 for c_comp in complex_content:
-#                     print('\t \t', c_comp.local_name)
-            
-
-# schema_iterate(nfe_schema)
-
-class DbBuilder:
     
-    def create_table(self, name: str, fields = {}):
-        #experimental para testes
-        print('Contruindo a tabela {table}'.format(table = name))
 
-    def create_field(self, table_name: str, field_name: str, type_name: str, type_length):
-        #experimental para testes
-        print('Criado o campo {field} na seguinte tabela {table}'.format(field=field_name,
-        table= table_name ))
+class Parser:
+    
+    @abc.abstractmethod
+    def parse(xmlShema: xmlschema.XMLSchema, handler: Handler):
+        pass
 
 
-def create_tables(xml_schema: xmlschema.XMLSchema, db_builder):
-    component_list = xml_schema.findall('.//*')
+class ParserDecorator(Parser):
 
-    # varredura dos componentes
-    for schema_component in component_list:
+    def __init__(self, wrappee_parser: Parser):
+        self.wrappee = wrappee_parser
+
+    def parse(self, xmlschema: xmlshema.XMLSchema, db_randler: Handler):
+        pass
+
