49609a7c8d33dbacf02e76350f73104dd9ac64ab
andreenigma <46359994+andreenigma@users.noreply.github.com>
2020-09-17 23:01:17 -0300

    Primeiros decoradores do parser

--- xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
+++ xml_nfe_to_sql/src/xml_nfe_to_sql/__init__.py
@@ -9,12 +9,12 @@ import pandas as pd
 import sqlalchemy
 
 
-nfe_v4_schema_file = open('./resourse/xsd/NFe/v3.10/nfe_v3.10.xsd')
-nfe_v4_base_url = './resourse/xsd/NFe/v3.10/'
+# nfe_v4_schema_file = open('./resourse/xsd/NFe/v3.10/nfe_v3.10.xsd')
+# nfe_v4_base_url = './resourse/xsd/NFe/v3.10/'
 
-nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= nfe_v4_base_url )
+# nfe_schema = xmlschema.XMLSchema(nfe_v4_schema_file, base_url= nfe_v4_base_url )
 
-component_list = nfe_schema.findall('.//*')
+# component_list = nfe_schema.findall('.//*')
    
 
 class Handler:
@@ -44,7 +44,8 @@ class Parser:
     
     @abc.abstractmethod
     def parse(xmlShema: xmlschema.XMLSchema, handler: Handler):
-        pass
+        print('C')
+
 
 
 class ParserDecorator(Parser):
@@ -53,5 +54,30 @@ class ParserDecorator(Parser):
         self.wrappee = wrappee_parser
 
     def parse(self, xmlschema: xmlshema.XMLSchema, db_randler: Handler):
-        pass
+        print('Executada funcao parse() da classe ParserDecorator base.')
 
+
+
+class UnormalizedTablesCreator(Parser):
+    """Classe a ser encapsulada por decoradores que chama função no handler para
+    criar tabelas SQL não normalizadas. A funcao parce() dessa classe deve ser a
+    primeira a ser executada na pilha de decoradores, já que cria as tabelas sobre
+    as quais os campos e relacionamentos do banco de dados serão criados.
+    """
+
+    def parse(self, xmlschema: xmlshema.XMLSchema, db_randler: Handler):
+        component_list = xmlschema.findall('.//*')
+        for xsd_component in component_list:
+            # faca alguma coisa
+
+
+class RelationalCreator(ParserDecorator):
+
+    def __init__(self, wrappee_parser: Parser):
+        super.__init__(wrappee_parser)
+
+    def parse(self, xmlschema: xmlshema.XMLSchema, db_randler: Handler):
+        self.wrappee.parse(xmlschema, db_randler)
+        component_list = xmlschema.findall('.//*')
+        for xsd_component in component_list:
+            # faca alguma coisa 
